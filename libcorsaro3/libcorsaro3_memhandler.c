93de4ae73f0d752a7017ae2d443245b779e37ccd
Shane Alcock <salcock@waikato.ac.nz>
2018-08-10 15:00:22 +1200

    Add some documentation for the memory handler code

--- libcorsaro3/libcorsaro3_memhandler.c
+++ libcorsaro3/libcorsaro3_memhandler.c
@@ -37,16 +37,94 @@
 #include "libcorsaro3_log.h"
 #include "libcorsaro3_memhandler.h"
 
+/** Corsaro memory handler
+ *  ======================
+ *
+ *  Efficient memory allocation and release for situations where you are
+ *  wanting to allocate (and free) instances of the exact same structure
+ *  repeatedly.
+ *
+ *  There is a surprising amount of overhead involved in each call to
+ *  malloc or free that you make. If you are dealing with 1000s (or millions)
+ *  of allocated instances of the same structure, it therefore makes much
+ *  more sense to bulk allocate contiguous blobs of memory that you can then
+ *  manually sub-divide and distribute as needed, i.e. one allocation
+ *  gives you enough structures to store 10,000 flowtuple results.
+ *
+ *  The other issue with malloc/free is that the memory seldom gets returned
+ *  to the OS before the process ends; it is usually cached within the
+ *  process just in case it is needed again. In the corsaro context, this
+ *  means that a single busy interval can cause our long-running process to
+ *  permanently have a larger than usual memory footprint.
+ *
+ *  The latter problem can be resolved by using mmap instead of malloc to
+ *  allocate memory. mmap itself is a somewhat coarse approach to memory
+ *  allocation, completely unsuitable for small memory allocations. Since we
+ *  are committed to doing large bulk allocations anyway, mmap works great
+ *  for us. Calls to munmap will release the memory back to the OS, so we
+ *  only need to worry about caching as much released memory as we think
+ *  we'll need in the general case.
+ *
+ *  How it works
+ *  ============
+ *  The core component of the memory handler system is the "blob". A blob is
+ *  simply a contiguous block of mmapped memory that is big enough to fit
+ *  a pre-configured number of instances of a particular structure, much like
+ *  an array.
+ *
+ *  When we first initialise a memory handler, we are given the size of the
+ *  structure that the user wants to bulk allocate and the number of instances
+ *  to allocate each time we call mmap. The new handler will use mmap to
+ *  allocate the first blob, which will be initialised as having zero items
+ *  used.
+ *
+ *  Whenever a user requests a new item from the memory handler, it first
+ *  checks to see if there are any unused items available in the most
+ *  recently allocated blob. If yes, it will return a pointer to the
+ *  first unused portion of the blob as well as a reference to the blob
+ *  itself (used later on to help release the item back to the blob). If
+ *  no, a new blob is allocated and the first item from that blob is
+ *  returned instead.
+ *
+ *  When a user has finished with the item that they had requested, they
+ *  call the release API method on the handler and the blob that the item
+ *  came from. The number of released items for the blob is incremented.
+ *  If we also determine that the blob is both not the most recently
+ *  allocated blob AND all of its items have been released back, we can
+ *  then finally munmap the blob and return its memory back to the OS.
+ *
+ *  In practice, we keep a few extra blobs around in a free list so we
+ *  aren't continually mmaping and munmaping blobs that we could just
+ *  recycle instead.
+
+ *  Since we are reference-counting only, we make no effort to validate
+ *  that the item being released to us is legit. It is the user's
+ *  responsibility to make sure that they do not release the same item
+ *  twice or release an item they did not request. The main reasons for
+ *  not adding that validation are
+ *    - this is an internal corsaro API, so any screw ups with this API
+ *      would be our own fault anyway.
+ *    - not having to keep track of which specific segments of a blob
+ *      have been released will save us quite a bit of memory and CPU
+ *      overhead, which was the point of adding the handler in the first
+ *      place.
+ */
+
 static inline corsaro_memsource_t *create_fresh_blob(uint32_t itemcount,
         size_t itemsize, corsaro_memhandler_t *handler) {
 
     corsaro_memsource_t *blob;
     size_t upsize;
 
+    /* Our allocation needs to be a multiple of the page size -- round up
+     * to the next page if need be.
+     */
     upsize = (((itemsize * itemcount) / handler->pagesize) + 1) * handler->pagesize;
 
     blob = (corsaro_memsource_t *)malloc(sizeof(corsaro_memsource_t));
 
+    /* Just need some anonymous writable memory that is limited to our process
+     * only */
     blob->blob = mmap(NULL, upsize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS,
                 -1, 0);
     if (blob->blob == MAP_FAILED) {
@@ -91,6 +169,8 @@ void destroy_corsaro_memhandler(corsaro_memhandler_t *handler) {
 
     corsaro_memsource_t *blob, *tmp;
 
+    /* Only actually destroy the memory handler if we are the last
+     * user left standing. */
     pthread_mutex_lock(&handler->mutex);
     handler->users --;
     if (handler->users > 0) {
@@ -132,9 +212,8 @@ uint8_t *get_corsaro_memhandler_item(corsaro_memhandler_t *handler,
     uint8_t *mem;
 
     /* If the current blob still have slots available, just return one
-     * of those.
-     * Otherwise, we need to create a new handler and grab the first slot
-     * from that.
+     * of those. Otherwise, we need to create a new blob and grab the first
+     * slot from that.
      */
 
     pthread_mutex_lock(&handler->mutex);
@@ -142,7 +221,7 @@ uint8_t *get_corsaro_memhandler_item(corsaro_memhandler_t *handler,
 
         if (handler->current->released == handler->current->alloceditems) {
             /* User has been releasing as fast as they've been allocating,
-             * just reuse current */
+             * just reuse current rather than bothering with the freelist */
             handler->current->nextavail = 0;
             handler->current->released = 0;
             handler->current->nextfree = NULL;
@@ -163,6 +242,7 @@ uint8_t *get_corsaro_memhandler_item(corsaro_memhandler_t *handler,
         }
     }
 
+    /* Find the next available slot in the current blob */
     mem = handler->current->blob + (handler->current->nextavail *
             handler->current->itemsize);
     handler->current->nextavail ++;
@@ -185,6 +265,7 @@ void release_corsaro_memhandler_item(corsaro_memhandler_t *handler,
 
     if (itemsource != handler->current &&
             itemsource->released == handler->items_per_blob) {
+        /* Put the blob on the freelist for possible re-use */
         assert(handler->freelist != itemsource);
         itemsource->nextfree = handler->freelist;
         handler->freelist = itemsource;
@@ -192,6 +273,8 @@ void release_corsaro_memhandler_item(corsaro_memhandler_t *handler,
         handler->unreleased --;
     }
 
+    /* Don't allow our freelist to grow too large -- may as well
+     * return the excess blobs back to the OS at this point. */
     while (handler->freelistavail > 100) {
         corsaro_memsource_t *tmp = handler->freelist;
         handler->freelist = handler->freelist->nextfree;
--- libcorsaro3/libcorsaro3_memhandler.h
+++ libcorsaro3/libcorsaro3_memhandler.h
@@ -37,44 +37,144 @@
 
 typedef struct corsaro_memblob corsaro_memsource_t;
 
+/** A large chunk of contiguous memory that is used to sub-allocate
+ *  memory for small frequently used structures.
+ */
 struct corsaro_memblob {
 
+    /** Pointer to the contiguous memory blob */
     uint8_t *blob;
+
+    /** Size of the contiguous memory blob */
     size_t blobsize;
+
+    /** Size of the structure that we are allocated out of this blob */
     size_t itemsize;
+
+    /** Number of structures that can be allocated out of this blob */
     uint32_t alloceditems;
 
+    /** The index of the next available sub-allocation */
     uint32_t nextavail;
+
+    /** Number of sub-allocations that have been released */
     uint32_t released;
 
+    /** Reference to the next blob in a handler's free list */
     corsaro_memsource_t *nextfree;
 };
 
+/** Custom memory allocator / manager that allows us to do bulk memory
+ *  allocations for structures that we would otherwise be frequently
+ *  allocating and freeing.
+ */
 typedef struct corsaro_memhandler {
 
+    /** Reference to a corsaro logger for error reporting */
     corsaro_logger_t *logger;
+
+    /** Number of structures that should be allocated in a single bulk alloc */
     uint32_t items_per_blob;
+
+    /** Size of the structure that is being allocated using this handler */
     size_t itemsize;
+
+    /** Number of threads that are currently using memory allocated by this
+     *  handler.
+     */
     int users;
+
+    /** Number of blobs that are currently available in the freelist */
     int freelistavail;
+
+    /** Mutex for protecting key fields against concurrent access */
     pthread_mutex_t mutex;
 
+    /** Reference to the blob that is currently being used for sub-allocations
+     */
     corsaro_memsource_t *current;
+
+    /** A free list containing blobs that have been entirely released back
+     *  to the handler and can therefore be reused.
+     */
     corsaro_memsource_t *freelist;
 
+    /** Number of blobs that have been allocated by this handler and not yet
+     *  completely released by the user.
+     */
     uint32_t unreleased;
+
+    /** Page size for the system running this memory handler -- our bulk
+     *  allocations need to be a multiple of the page size.
+     */
     size_t pagesize;
 
 } corsaro_memhandler_t;
 
+/** Initialises a new memory handler and allocates the first memory blob.
+ *
+ *  @param logger       A reference to a corsaro logger for error reporting
+ *  @param handler      The memory handler to be initialised
+ *  @param itemsize     The size of the structure that is to be bulk allocated
+ *                      by this handler.
+ *  @param itemsperalloc    The number of structures that should be allocated
+ *                          by each bulk allocation.
+ */
 void init_corsaro_memhandler(corsaro_logger_t *logger,
         corsaro_memhandler_t *handler, size_t itemsize,
         uint32_t itemsperalloc);
+
+/** Decrements the user count for a memory handler. If the user count reaches
+ *  zero, the memory handler will be destroyed.
+ *
+ *  @param handler      The memory handler to be destroyed.
+ *
+ *  @note Ideally, all blobs should have been fully released by the time you
+ *        call this for the last time, otherwise they may be leaked.
+ */
 void destroy_corsaro_memhandler(corsaro_memhandler_t *handler);
+
+/** Increments the user count for a memory handler. You will want to do this
+ *  once for every thread that is working with structures that have been
+ *  allocated by the memory handler.
+ *
+ *  @param handler      The memory handler that is to be shared with another
+ *                      thread.
+ */
 void add_corsaro_memhandler_user(corsaro_memhandler_t *handler);
 
+/** Requests a reference to a single, unused instance of the structure that is
+ *  being bulk allocated by a memory handler.
+ *
+ *  @param handler      The memory handler which will provide the new instance
+ *  @param itemsource[out]  Updated with a reference to the blob that the
+ *                          new instance has come from -- will be needed when
+ *                          it comes time to release the memory.
+ *  @return a pointer to memory that can be used to store an instance of the
+ *          structure managed by this handler.
+ *
+ *  It is *very* important that you keep track of 'itemsource' for every
+ *  instance you get from this function. I usually add a corsaro_memsource_t
+ *  pointer to my managed structure and set that to be 'itemsource' as soon
+ *  as this function returns.
+ */
 uint8_t *get_corsaro_memhandler_item(corsaro_memhandler_t *handler,
         corsaro_memsource_t **itemsource);
+
+/** Releases a reference to a single structure instance back to the memory
+ *  handler that originally provided it.
+ *
+ *  @param handler      The memory handler which originally provided the
+ *                      instance to be released.
+ *  @param itemsource   A reference to the blob that the released memory was
+ *                      allocated from.
+ *
+ *  @note You don't need to provide a pointer to the structure itself, as
+ *        internally we only count references requested and references
+ *        released. This means the user must be careful not to release the
+ *        same structure instance twice, because we have no information
+ *        available that would help detect this situation.
+ */
 void release_corsaro_memhandler_item(corsaro_memhandler_t *handler,
         corsaro_memsource_t *itemsource);
 
