53d0719478ebb52c043a3a1e71bd2845f6055227
Shane Alcock <salcock@waikato.ac.nz>
2019-03-05 19:09:06 -0800

    Add code for parsing and managing libtimeseries configuration

---
+++ libcorsaro/libcorsaro_libtimeseries.c
@@ -0,0 +1,502 @@
+/*
+ * corsaro
+ *
+ * Alistair King, CAIDA, UC San Diego
+ * Shane Alcock, WAND, University of Waikato
+ *
+ * corsaro-info@caida.org
+ *
+ * Copyright (C) 2012 The Regents of the University of California.
+ *
+ * This file is part of corsaro.
+ *
+ * corsaro is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * corsaro is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with corsaro.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <inttypes.h>
+#include "libcorsaro_libtimeseries.h"
+#include "libcorsaro_common.h"
+
+void init_libts_ascii_backend(libts_ascii_backend_t *back) {
+    back->filename = NULL;
+    back->compresslevel = 1;
+}
+
+void init_libts_kafka_backend(libts_kafka_backend_t *back) {
+    back->brokeruri = NULL;
+    back->channelname = NULL;
+    back->compresscodec = NULL;
+    back->topicprefix = NULL;
+}
+
+void init_libts_dbats_backend(libts_dbats_backend_t *back) {
+    back->path = NULL;
+    back->flags = 0;
+}
+
+void init_libts_tsmq_backend(libts_tsmq_backend_t *back) {
+    back->brokeruri = NULL;
+    back->retries = 3;
+    back->acktimeout = 60000;
+    back->lookuptimeout = 30 * 60 * 1000;
+    back->settimeout = 2 * 60 * 1000;
+}
+
+void destroy_libts_ascii_backend(libts_ascii_backend_t *back) {
+    if (back->filename) {
+        free(back->filename);
+    }
+}
+
+void destroy_libts_kafka_backend(libts_kafka_backend_t *back) {
+    if (back->brokeruri) {
+        free(back->brokeruri);
+    }
+    if (back->channelname) {
+        free(back->channelname);
+    }
+    if (back->compresscodec) {
+        free(back->compresscodec);
+    }
+    if (back->topicprefix) {
+        free(back->topicprefix);
+    }
+}
+
+void destroy_libts_dbats_backend(libts_dbats_backend_t *back) {
+    if (back->path) {
+        free(back->path);
+    }
+}
+
+void destroy_libts_tsmq_backend(libts_tsmq_backend_t *back) {
+    if (back->brokeruri) {
+        free(back->brokeruri);
+    }
+}
+
+int configure_libts_ascii_backend(corsaro_logger_t *logger,
+        libts_ascii_backend_t *back, yaml_document_t *doc, yaml_node_t *node) {
+
+    yaml_node_t *key, *value;
+    yaml_node_pair_t *pair;
+
+    if (node->type != YAML_MAPPING_NODE) {
+        corsaro_log(logger, "Libtimeseries ASCII backend config should be a map.");
+        return -1;
+    }
+
+    for (pair = node->data.mapping.pairs.start;
+            pair < node->data.mapping.pairs.top; pair ++) {
+
+        char *val;
+        key = yaml_document_get_node(doc, pair->key);
+        value = yaml_document_get_node(doc, pair->value);
+
+        val = (char *)value->data.scalar.value;
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "file") == 0) {
+            if (back->filename) {
+                free(back->filename);
+            }
+            back->filename = strdup(val);
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "compress") == 0) {
+            back->compresslevel = atoi(val);
+            if (back->compresslevel < 0 || back->compresslevel > 9) {
+                corsaro_log(logger, "Libtimeseries ASCII backend: compress value of %d is invalid", back->compresslevel);
+                corsaro_log(logger, "(must be 0-9 inclusive)");
+                corsaro_log(logger, "setting compress level to 1 instead");
+                back->compresslevel = 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int configure_libts_kafka_backend(corsaro_logger_t *logger,
+        libts_kafka_backend_t *back, yaml_document_t *doc, yaml_node_t *node) {
+
+    yaml_node_t *key, *value;
+    yaml_node_pair_t *pair;
+
+    if (node->type != YAML_MAPPING_NODE) {
+        corsaro_log(logger, "Libtimeseries Kafka backend config should be a map.");
+        return -1;
+    }
+
+    for (pair = node->data.mapping.pairs.start;
+            pair < node->data.mapping.pairs.top; pair ++) {
+
+        char *val;
+        key = yaml_document_get_node(doc, pair->key);
+        value = yaml_document_get_node(doc, pair->value);
+
+        val = (char *)value->data.scalar.value;
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "brokeruri") == 0) {
+            if (back->brokeruri) {
+                free(back->brokeruri);
+            }
+            back->brokeruri = strdup(val);
+        }
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "channelname") == 0) {
+            if (back->channelname) {
+                free(back->channelname);
+            }
+            back->channelname = strdup(val);
+        }
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "compresscodec") == 0) {
+            if (back->compresscodec) {
+                free(back->compresscodec);
+            }
+            back->compresscodec = strdup(val);
+        }
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "topicprefix") == 0) {
+            if (back->topicprefix) {
+                free(back->topicprefix);
+            }
+            back->topicprefix = strdup(val);
+        }
+    }
+    return 0;
+}
+
+int configure_libts_dbats_backend(corsaro_logger_t *logger,
+        libts_dbats_backend_t *back, yaml_document_t *doc, yaml_node_t *node) {
+
+    yaml_node_t *key, *value;
+    yaml_node_pair_t *pair;
+
+    if (node->type != YAML_MAPPING_NODE) {
+        corsaro_log(logger, "Libtimeseries DBATS backend config should be a map.");
+        return -1;
+    }
+
+    for (pair = node->data.mapping.pairs.start;
+            pair < node->data.mapping.pairs.top; pair ++) {
+
+        char *val;
+        key = yaml_document_get_node(doc, pair->key);
+        value = yaml_document_get_node(doc, pair->value);
+        uint8_t onoffopt = 0;
+
+        val = (char *)value->data.scalar.value;
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "path") == 0) {
+            if (back->path) {
+                free(back->path);
+            }
+            back->path = strdup(val);
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "compression") == 0) {
+            if (parse_onoff_option(logger, val, &onoffopt, "compression")
+                    == 0) {
+                if (onoffopt == 0) {
+                    back->flags |= DBATS_FLAGS_UNCOMPRESSED;
+                }
+            }
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "exclusive") == 0) {
+            if (parse_onoff_option(logger, val, &onoffopt, "exclusive")
+                    == 0) {
+                if (onoffopt == 1) {
+                    back->flags |= DBATS_FLAGS_EXCLUSIVE;
+                }
+            }
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "transactions") == 0) {
+            if (parse_onoff_option(logger, val, &onoffopt, "transactions")
+                    == 0) {
+                if (onoffopt == 0) {
+                    back->flags |= DBATS_FLAGS_NOTXN;
+                }
+            }
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "updatable") == 0) {
+            if (parse_onoff_option(logger, val, &onoffopt, "updatable")
+                    == 1) {
+                if (onoffopt == 0) {
+                    back->flags |= DBATS_FLAGS_UPDATABLE;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int configure_libts_tsmq_backend(corsaro_logger_t *logger,
+        libts_tsmq_backend_t *back, yaml_document_t *doc, yaml_node_t *node) {
+
+    yaml_node_t *key, *value;
+    yaml_node_pair_t *pair;
+
+    if (node->type != YAML_MAPPING_NODE) {
+        corsaro_log(logger, "Libtimeseries TSMQ backend config should be a map.");
+        return -1;
+    }
+
+    for (pair = node->data.mapping.pairs.start;
+            pair < node->data.mapping.pairs.top; pair ++) {
+
+        char *val;
+        key = yaml_document_get_node(doc, pair->key);
+        value = yaml_document_get_node(doc, pair->value);
+
+        val = (char *)value->data.scalar.value;
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "brokeruri") == 0) {
+            if (back->brokeruri) {
+                free(back->brokeruri);
+            }
+            back->brokeruri = strdup(val);
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "retries") == 0) {
+            back->retries = atoi(val);
+            if (back->retries < 0) {
+                corsaro_log(logger, "Libtimeseries TSMQ backend: retries value of %d is invalid", back->retries);
+                corsaro_log(logger, "(must be non-negative");
+                corsaro_log(logger, "setting retries value to 3 instead");
+                back->retries = 3;
+            }
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "acktimeout") == 0) {
+            back->acktimeout = atoi(val);
+            if (back->acktimeout < 0) {
+                corsaro_log(logger, "Libtimeseries TSMQ backend: ACK timeout value of %d is invalid", back->acktimeout);
+                corsaro_log(logger, "(must be non-negative");
+                corsaro_log(logger, "setting ACK timeout value to 5 sec instead");
+                back->acktimeout = 5000;
+            }
+        }
+
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "lookuptimeout") == 0) {
+            back->lookuptimeout = atoi(val);
+            if (back->lookuptimeout < 0) {
+                corsaro_log(logger, "Libtimeseries TSMQ backend: lookup timeout of %d is invalid", back->lookuptimeout);
+                corsaro_log(logger, "(must be non-negative");
+                corsaro_log(logger, "setting lookup timeout to 60 sec instead");
+                back->lookuptimeout = 60 * 1000;
+            }
+        }
+        if (key->type == YAML_SCALAR_NODE && value->type == YAML_SCALAR_NODE
+                && strcmp((char *)key->data.scalar.value, "settimeout") == 0) {
+            back->settimeout = atoi(val);
+            if (back->settimeout < 0) {
+                corsaro_log(logger, "Libtimeseries TSMQ backend: set timeout of %d is invalid", back->settimeout);
+                corsaro_log(logger, "(must be non-negative");
+                corsaro_log(logger, "setting set timeout to 30 sec instead");
+                back->settimeout = 30 * 1000;
+            }
+        }
+    }
+    return 0;
+}
+
+#define ADD_TO_STRING(ptr, opt, limit, backend) \
+    if (strlen(opt) > limit - ptr) { \
+        corsaro_log(logger, "Libtimeseries %s backend: arguments exceed 4096 bytes in total?", backend); \
+        corsaro_log(logger, " - disabling %s backend", backend); \
+        return NULL; \
+    } \
+    strncat(ptr, opt, limit-ptr); \
+    ptr += strlen(opt);
+
+char *create_libts_ascii_option_string(corsaro_logger_t *logger,
+        libts_ascii_backend_t *back) {
+
+    char tmpbuf[4096];
+    char opt[1024];
+    char *ptr = tmpbuf;
+    char *limit = tmpbuf + 4096 - 1;
+
+    if (back->filename == NULL) {
+        return NULL;
+    }
+
+    if (snprintf(opt, 1024, "-f %s ", back->filename) >= 1024) {
+        corsaro_log(logger, "Overly large filename for libtimeseries ASCII backend");
+        corsaro_log(logger, " -- disabling ASCII backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "ASCII");
+
+    if (snprintf(opt, 1024, "-c %d ", back->compresslevel) >= 1024) {
+        corsaro_log(logger, "Overly large numeric value for libtimeseries ASCII backend (compress level)");
+        corsaro_log(logger, " -- disabling ASCII backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "ASCII");
+    return strdup(tmpbuf);
+}
+
+char *create_libts_kafka_option_string(corsaro_logger_t *logger,
+        libts_kafka_backend_t *back) {
+
+    char tmpbuf[4096];
+    char opt[1024];
+    char *ptr = tmpbuf;
+    char *limit = tmpbuf + 4096 - 1;
+
+    if (back->brokeruri == NULL) {
+        return NULL;
+    }
+
+    if (snprintf(opt, 1024, "-b %s ", back->brokeruri) >= 1024) {
+        corsaro_log(logger, "Overly large broker URI for libtimeseries Kafka backend");
+        corsaro_log(logger, " -- disabling Kafka backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "Kafka");
+
+    if (snprintf(opt, 1024, "-c %s ", back->channelname) >= 1024) {
+        corsaro_log(logger, "Overly large channel name for libtimeseries Kafka backend");
+        corsaro_log(logger, " -- disabling Kafka backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "Kafka");
+
+    if (snprintf(opt, 1024, "-C %s ", back->compresscodec) >= 1024) {
+        corsaro_log(logger, "Overly large compression codec for libtimeseries Kafka backend");
+        corsaro_log(logger, " -- disabling Kafka backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "Kafka");
+
+    if (snprintf(opt, 1024, "-p %s ", back->topicprefix) >= 1024) {
+        corsaro_log(logger, "Overly large topic prefix for libtimeseries Kafka backend");
+        corsaro_log(logger, " -- disabling Kafka backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "Kafka");
+    return strdup(tmpbuf);
+}
+
+char *create_libts_dbats_option_string(corsaro_logger_t *logger,
+        libts_dbats_backend_t *back) {
+
+    char tmpbuf[4096];
+    char opt[1024];
+    char *ptr = tmpbuf;
+    char *limit = tmpbuf + 4096 - 1;
+
+    if (back->path == NULL) {
+        return NULL;
+    }
+
+    if (snprintf(opt, 1024, "-p %s ", back->path) >= 1024) {
+        corsaro_log(logger, "Overly large path for libtimeseries DBATS backend - disabling DBATS backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "DBATS");
+
+    if (back->flags & DBATS_FLAGS_UNCOMPRESSED) {
+        ADD_TO_STRING(ptr, "-f FLAG_UNCOMRESSED ", limit, "DBATS");
+    }
+
+    if (back->flags & DBATS_FLAGS_EXCLUSIVE) {
+        ADD_TO_STRING(ptr, "-f FLAG_EXCLUSIVE ", limit, "DBATS");
+    }
+
+    if (back->flags & DBATS_FLAGS_NOTXN) {
+        ADD_TO_STRING(ptr, "-f FLAG_NO_TXN ", limit, "DBATS");
+    }
+
+    if (back->flags & DBATS_FLAGS_UPDATABLE) {
+        ADD_TO_STRING(ptr, "-f FLAG_UPDATABLE ", limit, "DBATS");
+    }
+    return strdup(tmpbuf);
+}
+
+char *create_libts_tsmq_option_string(corsaro_logger_t *logger,
+        libts_tsmq_backend_t *back) {
+
+    char tmpbuf[4096];
+    char opt[1024];
+    char *ptr = tmpbuf;
+    char *limit = tmpbuf + 4096 - 1;
+
+    if (back->brokeruri == NULL) {
+        return NULL;
+    }
+
+    if (snprintf(opt, 1024, "-b %s ", back->brokeruri) >= 1024) {
+        corsaro_log(logger, "Overly large broker URI for libtimeseries TSMQ backend -- disabling TSMQ backend");
+        return NULL;
+    }
+
+    ADD_TO_STRING(ptr, opt, limit, "TSMQ");
+
+    if (snprintf(opt, 1024, "-r %d ", back->retries) >= 1024) {
+        corsaro_log(logger, "Overly large numeric option for libtimeseries TSMQ backend (retries)");
+        corsaro_log(logger, " -- disabling TSMQ backend");
+        return NULL;
+    }
+    ADD_TO_STRING(ptr, opt, limit, "TSMQ");
+
+    if (snprintf(opt, 1024, "-a %d ", back->acktimeout) >= 1024) {
+        corsaro_log(logger, "Overly large numeric option for libtimeseries TSMQ backend (acktimeout)");
+        corsaro_log(logger, " -- disabling TSMQ backend");
+        return NULL;
+    }
+    ADD_TO_STRING(ptr, opt, limit, "TSMQ");
+
+    if (snprintf(opt, 1024, "-l %d ", back->lookuptimeout) >= 1024) {
+        corsaro_log(logger, "Overly large numeric option for libtimeseries TSMQ backend (lookuptimeout)");
+        corsaro_log(logger, " -- disabling TSMQ backend");
+        return NULL;
+    }
+    ADD_TO_STRING(ptr, opt, limit, "TSMQ");
+
+    if (snprintf(opt, 1024, "-s %d ", back->settimeout) >= 1024) {
+        corsaro_log(logger, "Overly large numeric option for libtimeseries TSMQ backend (settimeout)");
+        corsaro_log(logger, " -- disabling TSMQ backend");
+        return NULL;
+    }
+    ADD_TO_STRING(ptr, opt, limit, "TSMQ");
+
+    return strdup(tmpbuf);
+}
+
+// vim: set sw=4 tabstop=4 softtabstop=4 expandtab :
---
+++ libcorsaro/libcorsaro_libtimeseries.h
@@ -0,0 +1,102 @@
+/*
+ * corsaro
+ *
+ * Alistair King, CAIDA, UC San Diego
+ * Shane Alcock, WAND, University of Waikato
+ *
+ * corsaro-info@caida.org
+ *
+ * Copyright (C) 2012 The Regents of the University of California.
+ *
+ * This file is part of corsaro.
+ *
+ * corsaro is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * corsaro is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with corsaro.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef LIBCORSARO_LIBTIMESERIES_H_
+#define LIBCORSARO_LIBTIMESERIES_H_
+
+#include "libcorsaro_log.h"
+#include <inttypes.h>
+#include <yaml.h>
+
+enum {
+    DBATS_FLAGS_UNCOMPRESSED,
+    DBATS_FLAGS_EXCLUSIVE,
+    DBATS_FLAGS_NOTXN,
+    DBATS_FLAGS_UPDATABLE
+};
+
+typedef struct libts_ascii_backend {
+    char *filename;         /* NULL if inactive */
+    int compresslevel;
+} libts_ascii_backend_t;
+
+typedef struct libts_kafka_backend {
+    char *brokeruri;        /* NULL if inactive */
+    char *channelname;
+    char *compresscodec;
+    char *topicprefix;
+} libts_kafka_backend_t;
+
+typedef struct libts_dbats_backend {
+    char *path;             /* NULL if inactive */
+    uint32_t flags;
+} libts_dbats_backend_t;
+
+typedef struct libts_tsmq_backend {
+    char *brokeruri;        /* NULL if inactive */
+    int retries;
+    int acktimeout;
+    int lookuptimeout;
+    int settimeout;
+} libts_tsmq_backend_t;
+
+void init_libts_ascii_backend(libts_ascii_backend_t *back);
+void init_libts_kafka_backend(libts_kafka_backend_t *back);
+void init_libts_dbats_backend(libts_dbats_backend_t *back);
+void init_libts_tsmq_backend(libts_tsmq_backend_t *back);
+
+int configure_libts_ascii_backend(corsaro_logger_t *logger,
+        libts_ascii_backend_t *back,
+        yaml_document_t *doc, yaml_node_t *node);
+int configure_libts_kafka_backend(corsaro_logger_t *logger,
+        libts_kafka_backend_t *back,
+        yaml_document_t *doc, yaml_node_t *node);
+int configure_libts_dbats_backend(corsaro_logger_t *logger,
+        libts_dbats_backend_t *back,
+        yaml_document_t *doc, yaml_node_t *node);
+int configure_libts_tsmq_backend(corsaro_logger_t *logger,
+        libts_tsmq_backend_t *back,
+        yaml_document_t *doc, yaml_node_t *node);
+
+void destroy_libts_ascii_backend(libts_ascii_backend_t *back);
+void destroy_libts_kafka_backend(libts_kafka_backend_t *back);
+void destroy_libts_dbats_backend(libts_dbats_backend_t *back);
+void destroy_libts_tsmq_backend(libts_tsmq_backend_t *back);
+
+char *create_libts_ascii_option_string(corsaro_logger_t *logger,
+        libts_ascii_backend_t *back);
+char *create_libts_kafka_option_string(corsaro_logger_t *logger,
+        libts_kafka_backend_t *back);
+char *create_libts_dbats_option_string(corsaro_logger_t *logger,
+        libts_dbats_backend_t *back);
+char *create_libts_tsmq_option_string(corsaro_logger_t *logger,
+        libts_tsmq_backend_t *back);
+
+#endif
+
+// vim: set sw=4 tabstop=4 softtabstop=4 expandtab :
