7c8bc106fdffc3ff9cc05d2e84d7e95c8efb9b6e
Shane Alcock <salcock@waikato.ac.nz>
2020-03-10 19:31:56 -0700

    Add "nullplugin" plugin
    The plugin does nothing except count packets received by each
    of its processing threads. It's useful for profiling performance
    issues because it forces packets to go through
    the tagger's queues and into corsarotrace (as the plugin is a
    client that wants them) while being able to consume the packets as
    fast as our code allows since it does not do any processing or
    merging work.

---
+++ libcorsaro/plugins/corsaro_null.h
@@ -0,0 +1,11 @@
+#ifndef CORSARO_NULL_PLUGIN_H_
+#define CORSARO_NULL_PLUGIN_H_
+
+#include "libcorsaro.h"
+#include "libcorsaro_plugin.h"
+
+corsaro_plugin_t *corsaro_null_alloc(void);
+CORSARO_PLUGIN_GENERATE_PROTOTYPES(corsaro_null);
+
+
+#endif
